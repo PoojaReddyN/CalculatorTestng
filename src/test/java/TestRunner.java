751b6126a7253fda406d15db873502d1e937b61f
Pooja <nuthula_pooja@epam.com>
2025-02-11 17:00:24 +0530

    added files

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,2 +1,23 @@
+import org.testng.TestNG;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+import org.testng.xml.XmlClass;
+import java.util.Collections;
+
 public class TestRunner {
+    public static void main(String[] args) {
+
+        TestNG testNG = new TestNG();
+
+        XmlSuite suite = new XmlSuite();
+        suite.setName("Calculator Test Suite");
+
+        XmlTest test = new XmlTest(suite);
+        test.setName("Calculator Tests");
+        test.setXmlClasses(Collections.singletonList(new XmlClass("CalculatorTesting")));
+
+        testNG.setXmlSuites(Collections.singletonList(suite));
+
+        testNG.run();
+    }
 }
